81e40e674ff9c5328c2aef3ca127600880fcdb52
Elhadji Ngom <elhadji.ngom@aims-senegal.org>
2022-06-23 23:19:07 +0200

    Update deploy_app_neo4j_config.sh

--- deploy_app_neo4j_config.sh
+++ deploy_app_neo4j_config.sh
@@ -3,45 +3,45 @@
 # Made to set up ubuntu ec2 instance for konvo applications deployment!
 
 # to install neo4j on ubuntu
-sudo curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/neo4j.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/neo4j.gpg] https://debian.neo4j.com stable 4.3" | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+# sudo curl -fsSL https://debian.neo4j.com/neotechnology.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/neo4j.gpg
+# sudo echo "deb [signed-by=/usr/share/keyrings/neo4j.gpg] https://debian.neo4j.com stable 4.3" | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 
 # to update instance newly lunched
 sudo apt update
 sudo apt upgrade
 
 # to install neo4j
-sudo apt install neo4j -y
+# sudo apt install neo4j -y
 
 # to enable and start neo4j service
-sudo systemctl enable neo4j.service
-sudo systemctl start neo4j.service
-sudo systemctl status neo4j.service
+# sudo systemctl enable neo4j.service
+# sudo systemctl start neo4j.service
+# sudo systemctl status neo4j.service
 
 # to clone the app github repository
-# sudo git clone https://github.com/engom/nlp-app.git
-#cd nlp-app
+sudo git clone https://github.com/engom/nlp-app.git
+cd nlp-app
 
 # to install pip & virtualenv python modules
-#python3 -m install pip -y
-#python3 -m pip install virtualenv -y
-#python3 -m virtualenv env_app
+sudo python3 -m install pip -y
+sudo python3 -m pip install virtualenv -y
+sudo python3 -m virtualenv env_app
 
 # to activate the virtual env created
-#source env_app/bin/activate
+sudo source env_app/bin/activate
 
 # to install app_v2 requirements on env_app
-#python3 -m pip install -r requirements.text -y
+sudo python3 -m pip install -r requirements.text -y
 
 # to install awscli for s3 access
 #sudo apt-get update
 #sudo apt-get install awscli -y
 
 # to copy models weights from s3 bucket like this :
-# aws s3 cp s3://my_bucket/my_folder/my_file.ext my_copied_file.ext
+aws s3 cp s3://my_bucket/my_folder/my_file.ext my_copied_file.ext
 
-#aws s3 cp s3://konvo-models-store/sentimental_model_cmbert ./sentimental_model_cmbert
-#aws s3 cp s3://konvo-models-store/emotional_model_cmbert ./emotional_model_cmbert
+aws s3 cp s3://konvo-models-store/sentimental_model_cmbert ./sentimental_model_cmbert
+aws s3 cp s3://konvo-models-store/emotional_model_cmbert ./emotional_model_cmbert
 
 # to install tmux for background terminal run
 sudo apt update
